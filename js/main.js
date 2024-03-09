a1a5ab49778704d158ac279b0409a2c08b8ad41f
U2023 <kaedemau@outlook.com>
2024-03-09 11:14:25 +0900

    トグルメニューのウィンドウリサイズ対応

--- js/main.js
+++ js/main.js
@@ -1,24 +1,39 @@
 window.onload = () => {
+  toggleMenu();
+
+  window.addEventListener("resize", resize);
+};
+
+const toggleMenu = () => {
   if (window.innerWidth < 768) {
-    // トグルメニューボタンを追加
-    let element = document.getElementById("menu");
-    element.insertAdjacentHTML(
-      "beforebegin",
-      '<button id="toggle-menu" class="open-line"><div></div></button>'
-    );
-
     const toggle = document.getElementById("toggle-menu");
-    const menus = document.querySelectorAll("header #menu li");
-    const menuLines = document.querySelectorAll(".open-line");
+    if (!toggle) {
+      // トグルメニューボタンを追加
+      let element = document.getElementById("menu");
+      element.insertAdjacentHTML(
+        "beforebegin",
+        '<button id="toggle-menu" class="open-line"><div></div></button>'
+      );
 
-    toggle.addEventListener("click", () => {
-      menus.forEach((x) => (x.style.display = x.style.display == "block" ? "none" : "block"));
-      toggle.classList.contains("open-line")
-        ? toggle.classList.remove("open-line")
-        : toggle.classList.add("open-line");
-      toggle.classList.contains("close-line")
-        ? toggle.classList.remove("close-line")
-        : toggle.classList.add("close-line");
-    });
+      const toggle = document.getElementById("toggle-menu");
+      const menus = document.querySelectorAll("header #menu li");
+
+      toggle?.addEventListener("click", () => {
+        menus.forEach((x) => (x.style.display = x.style.display == "block" ? "none" : "block"));
+        toggle.classList.contains("open-line")
+          ? toggle.classList.remove("open-line")
+          : toggle.classList.add("open-line");
+        toggle.classList.contains("close-line")
+          ? toggle.classList.remove("close-line")
+          : toggle.classList.add("close-line");
+      });
+    }
+  } else {
+    const toggle = document.getElementById("toggle-menu");
+    toggle?.remove();
   }
+};
+
+const resize = () => {
+  toggleMenu();
 };
